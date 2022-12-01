f0bddecd8f9f69f83ea8130a9d91b1dbbb75013c
Ben Krepp <bkrepp@ctps.org>
2022-12-01 10:48:42 -0500

    CTPS PnR lots now analyzed as (centroid) point data.

--- process_polygon_layers.py
+++ process_polygon_layers.py
@@ -31,9 +31,6 @@ tracts_raw = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data
 #      Note: Be sure to use data projected to "Mass State Plane NAD83 Meters", so linear units will be in _meters_!
 tracts = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\TEPops_tracts_2020_MPO.gdb\\TEPops_tracts_2020_MPO_NA_clip_EPSG26986"
 
-# 3. Park-and-Ride Lots polygons
-CTPS_PNR_Lots_Polygons = "\\\\lindalino2\users\Public\Documents\Public ArcGIS\Database Connections\CTPS 10.6.sde\mpodata.mpodata.CTPS_PNR_Lots_Polygons"
-
 # Output feature classes
 ej_factors = [ "minority" , "lowinc", "lep", "disability", "elderly", "youth" ]
 
@@ -59,19 +56,3 @@ for ej_factor in ej_factors:
     arcpy.CalculateField_management(output_tbl, "area_sqmi", "!SUM_Shape_Area! / 2589988.1103", "PYTHON_9.3", "")
     arcpy.TableToTable_conversion(output_tbl, csv_output_dir, output_csv_fn)
 #
-
-# Analyze PNR Polygons separately - no selection of features is required
-s = 'Processing CTPS PNR polygon layer'
-arcpy.AddMessage(s)
-# Intermediate (temp) data
-ident_output = output_gdb + "\\" + "ctps_pnr_lots_fc"
-# Final output table
-pnr_output = output_gdb + "\\" + "ctps_pnr_lots_stats"
-# Process: Identity
-arcpy.Identity_analysis(CTPS_PNR_Lots_Polygons, probability_score_2050, ident_output, "ALL", "", "NO_RELATIONSHIPS")
-# Process: Summary Statistics
-arcpy.Statistics_analysis(ident_output, pnr_output, "shape_Area SUM", "score")
-# Process: Add Field for inundated area, in square miles
-arcpy.AddField_management(pnr_output, "area_sqmi", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-# Process: Calculate Field - convert area in square meters to square miles
-arcpy.CalculateField_management(pnr_output, "area_sqmi", "!SUM_shape_Area! / 2589988.1103", "PYTHON_9.3", "")
