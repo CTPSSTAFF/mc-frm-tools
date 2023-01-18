b65574423981fc0e9783c58c9ec470f8ed95ab53
Ben Krepp <bkrepp@ctps.org>
2023-01-18 11:27:14 -0500

    Added freight data elements, from Judy Taylor.

--- process_line_layers.py
+++ process_line_layers.py
@@ -29,17 +29,30 @@ arcpy.AddMessage('Classification shapefile: ' + probability_score_2050)
 mbta_rt = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\reference_data.gdb\\MGIS_MBTA_ARC_clip_dissolve"
 # 3. Commuter rail lines - clipped to TOWNSSURVEY_POLY, selecting records with coastal_poly = 'NO'
 mbta_cr = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\reference_data.gdb\\MGIS_TRAINS_RTE_TRAIN_clip"
-# 4. Road Inventory 2020
-# road_inv = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\RoadInventory2020.gdb\\RoadInventory2020"
-# 5. Road Inventory 2021
+# 4. Road Inventory 2021
 road_inv_2021 = r"\\lindalino2\apollo\mpodata\data\roads_gdb\RoadInv2021_and_pavement.gdb\RoadInventory"
 
-input_fcs = [mbta_rt, mbta_cr, road_inv_2021]
-aggregation_field_lists = [ "line;score", "comm_line;score",  "F_Class;score"  ]
+# 5. MassDOT Rail Inventory
+rail_inventory = "\\\\lindalino2\users\Public\Documents\Public ArcGIS\Database Connections\CTPS 10.6.sde\mpodata.mpodata.MASSDOT_RAIL_INVENTORY"
+# 6. National highway system freight network - Critical Urban Corridors
+nhfn_critical_urban = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\NHFN_MA.gdb\\NHFN_Critical_Urban_Corridors111219"
+# 7. National highway system freight network - Critical Rural Corridors
+nhfn_critical_rural = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\NHFN_MA.gdb\\NHFN_Critical_Rural_Corridors111219"
+# 8. National highway system freight network - Primary Highway Freight System
+nhfn_phfs = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\NHFN_MA.gdb\\NHFN_PHFS_111219"
+# 9. National highway system freight network - Non-Primary Highway Freight System
+nhfn_non_phfs = "G:\\Certification_Activities\\2023 LRTP Destination 2050\\GIS_Data\\MC_FRM_Analysis\\NHFN_MA.gdb\\NHFN_NON_PHFS_111219"
 
+input_fcs = [ mbta_rt, mbta_cr, road_inv_2021, 
+              rail_inventory, nhfn_critical_urban, nhfn_critical_rural, nhfn_phfs, nhfn_non_phfs ]
+          
+aggregation_field_lists = [ "line;score", "comm_line;score", "F_Class;score",
+                            "freight_op;ownership;score", "ROUTE_NAME;score", "ROUTE_NAME;score", "ROUTE_ID;ROAD_CLASS;score", "ROUTE_ID;ROAD_CLASS;score" ]
+                 
 # Output feature classes
-temp = [ "mbta_rapid_transit", "commuter_rail",  "road_inventory_2021" ]
-
+temp = [ "mbta_rapid_transit", "commuter_rail", "road_inventory_2021",
+         "rail_inventory", "nhfn_critical_urban", "nhfn_critical_rural", "nhfn_phfs", "nhfn_non_phfs" ]
+         
 output_fcs = [ output_gdb + "\\" + fc + "_fc" for fc in temp ]
 # Output tables
 output_tbls = [ output_gdb + "\\" + fc + "_stats" for fc in temp ]
